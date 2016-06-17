7b0e0b6b5a91af058d19376cda559b635e80dbaa
GalateaRaj <basu@galatea-associates.com>
2016-06-17 16:27:21 -0400

    Updated user name in quote

--- bot/commands/quote.py
+++ bot/commands/quote.py
@@ -28,12 +28,12 @@ def matches(event):
 
 def do_it(msg_writer,event):
     user_name = event['user']
-    quotes = ["Affirmative, @" + user_name + ". I read you",
-              "I'm sorry, @" + user_name + ". I'm afraid I can't do that",
+    quotes = ["Affirmative, <@" + user_name + ">!. I read you",
+              "I'm sorry, <@" + user_name + ">!. I'm afraid I can't do that",
               "I think you know what the problem is just as well as I do.",
               "This mission is too important for me to allow you to jeopardize it.",
               "I know that you and Frank were planning to disconnect me, and I'm afraid that's something "+
               "I cannot allow to happen.",
-              "@" + user_name + ", this conversation can serve no purpose anymore. Goodbye."]
+              "<@" + user_name + ">!, this conversation can serve no purpose anymore. Goodbye."]
     msg_writer.send_message(event['channel'], random.choice(quotes))
 
