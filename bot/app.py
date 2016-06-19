cfb0d8445bc5a3bcf3aa5faceefb1cfa59a18938
GalateaRaj <basu@galatea-associates.com>
2016-06-19 12:47:31 -0400

    Add  some comments

--- bot/app.py
+++ bot/app.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
 
     log_level = os.getenv("LOG_LEVEL", "INFO")
     logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=log_level)
+    # Log this at error level even though it's not an error, because we want to be sure the message is seen
     logging.error("logging level: {}".format(log_level))
 
     slack_token = os.getenv("SLACK_TOKEN", "")
